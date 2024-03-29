d05c3dad8845dbbbebc710dc40835e9aa863a26e
jhajel <hajelj@sfusd.edu>
2024-03-29 11:38:34 -0700

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024sfr",
+      "defaultValue": "2024cabe",
       "required": "true"
     },
     { "name": "Match Level",
